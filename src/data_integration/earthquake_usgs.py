95c775aa143e03f69b9dce8ac1b4c6aab8a84a84
gs-costa <gsantosaero@gmail.com>
2025-09-30 01:19:08 +0100

    feat: modularize ingest_metadata

--- src/data_integration/earthquake_usgs.py
+++ src/data_integration/earthquake_usgs.py
@@ -1,3 +1,4 @@
+import uuid
 from datetime import datetime
 
 from src.api.clients.usgs_earthquake_client import USGSEarthquakeClient
@@ -7,43 +8,52 @@ from src.app.repositories.database_repository import DatabaseRepository
 from src.logger import Logger
 
 
-def main(start_time: str, end_time: str):
-    """Main function demonstrating USGS Earthquake API usage."""
+class EarthquakeUSGSETL:
     logger = Logger(__name__)
 
-    client = USGSEarthquakeClient()
+    def __init__(self):
+        self.client = USGSEarthquakeClient()
+        self.db_session = SessionLocal()
 
-    db_session = SessionLocal()
+    def ingest_metadata(self, metadata: dict) -> uuid.UUID:
+        generated_timestamp = unix_timestamp_to_datetime(metadata.get("generated", 0))
 
-    try:
-        response = client.query_earthquakes(start_time=start_time, end_time=end_time, format_type="geojson")
-
-        if response.status_code == 200:
-            data = response.json()
-            logger.info(f"Total earthquakes found: {data.get('metadata', {}).get('count', 0)}")
-
-            metadata = data.get("metadata", {})
-
-            generated_timestamp = unix_timestamp_to_datetime(metadata.get("generated", 0))
-
-            metadata_db = Metadata(
-                generated=generated_timestamp,
-                url=metadata.get("url", ""),
-                title=metadata.get("title", ""),
-                status=metadata.get("status", 0),
-                api=metadata.get("api", ""),
-                count=metadata.get("count", 0),
-            )
-            metadata_repository = DatabaseRepository(Metadata, db_session)
+        metadata_db = Metadata(
+            generated=generated_timestamp,
+            url=metadata.get("url", ""),
+            title=metadata.get("title", ""),
+            status=metadata.get("status", 0),
+            api=metadata.get("api", ""),
+            count=metadata.get("count", 0),
+        )
+        metadata_repository = DatabaseRepository(Metadata, self.db_session)
+        try:
             metadata_repository.create(metadata_db)
-        else:
-            logger.error(f"Error: {response.status_code} - {response.text}")
+            return metadata_db.id
+        except Exception as e:
+            self.logger.error(f"Error ingesting metadata: {e}")
+            raise e
 
-    except Exception as e:
-        logger.error(f"Error occurred: {e}")
+    def main(self, start_time: str, end_time: str):
+        """Main function demonstrating USGS Earthquake API usage."""
 
-    finally:
-        client.close()
+        try:
+            response = self.client.query_earthquakes(start_time=start_time, end_time=end_time, format_type="geojson")
+
+            if response.status_code == 200:
+                data = response.json()
+                self.logger.info(f"Total earthquakes found: {data.get('metadata', {}).get('count', 0)}")
+
+                metadata_id = self.ingest_metadata(data.get("metadata", {}))
+                self.logger.info(f"Metadata ingested with ID: {metadata_id}")
+            else:
+                self.logger.error(f"Error: {response.status_code} - {response.text}")
+
+        except Exception as e:
+            self.logger.error(f"Error occurred: {e}")
+
+        finally:
+            self.client.close()
 
 
 def unix_timestamp_to_datetime(unix_timestamp: int) -> datetime:
@@ -51,4 +61,4 @@ def unix_timestamp_to_datetime(unix_timestamp: int) -> datetime:
 
 
 if __name__ == "__main__":
-    main(start_time="2024-01-01", end_time="2024-01-02")
+    EarthquakeUSGSETL().main(start_time="2024-01-01", end_time="2024-01-02")
